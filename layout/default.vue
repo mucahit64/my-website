a0cd09c5b7551626c646a050b1806f0579abb97c
Mücahit Yaman <mucahit.yaman@eliarge.com>
2024-07-09 00:25:45 +0300

    feat: The foundations of the project were laid.

---
+++ layout/default.vue
@@ -0,0 +1,21 @@
+  <script setup lang="ts">
+  </script>
+  
+  <template>
+  <NuxtLayout>
+    <div>
+      <header>
+        <h1>My Nuxt App</h1>
+        <nav>
+          <div>Home</div>
+          <div>About</div>
+        </nav>
+      </header>
+      <NuxtPage />
+      <footer>
+        <p>&copy; 2024 My Nuxt App</p>
+      </footer>
+    </div>
+  </NuxtLayout>
+  </template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<NuxtLayout>
+<div>
+    <h2>Home Page</h2>
+    <p>Welcome to the home page!</p>
+</div>
+<NuxtPage />
+</NuxtLayout>
+</template>
